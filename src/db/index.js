1f4f08651aa96a03f57d1b6146378ac34fb5e29a
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-11-09 13:15:20 +0000

    1

---
+++ src/db/index.js
@@ -0,0 +1,65 @@
+//bd.js
+import roteadorLogin from "./routes/login.js";
+
+import pkg from "pg";
+const { Pool } = pkg;
+
+app.use(roteadorLogin);
+
+async function connect() {
+    const pool = new Pool({
+      connectionString: process.env.URL_BD,
+    });
+    return pool.connect();
+  }
+
+  async function selectUsuarios() {         // Mudar para o nome certo dps!!!
+    const client = await connect();
+    const res = await client.query("SELECT * FROM usuario");
+    return res.rows;
+  }
+
+  //bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  return res.rows;
+}
+
+//bd.js
+async function insertUsuario(data) {
+    const client = await connect();
+    const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+    const usuario = [data.nome, data.senha, data.email];
+    await client.query(query, usuario);
+  }
+  
+  //bd.js
+  async function deleteUsuario(id) {
+    const client = await connect();
+    const query = "DELETE FROM usuario WHERE id = $1";
+    await client.query(query, [id]);
+  }
+  
+  //bd.js
+  async function updateUsuario(data) {
+    const client = await connect();
+    const query =
+      "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+    const usuario = [data.nome, data.email, data.senha, data.id];
+    await client.query(query, usuario);
+  }
+
+  // src/db/index.js
+async function autenticarUsuario(email, senha) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE email = $1 AND senha = $2";
+  const usuario = [email, senha];
+  const res = await client.query(query, usuario);
+  return res.rows[0];
+}
+
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario, autenticarUsuario };
+
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import roteadorUsuario from "./routes/usuario.js"; //importa router
+dotenv.config();
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API para CRUD usuario: https://github.com/Davilucasanteves/api-node-express.git",
+  });
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(roteadorUsuario);  //routerUsuario
+
+/*
+
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./db/index.js";
+
+//index.js
+app.use(express.json());
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.delete("/usuario/:id", async (req, res) => {
+  console.log("Rota DELETE /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) {
+      await deleteUsuario(req.params.id);
+      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+//index.js
+app.patch("/usuario", async (req, res) => {
+  console.log("Rota PATCH /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.body.id);
+    if (usuario.length > 0) {
+      await updateUsuario(req.body);
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+*/
